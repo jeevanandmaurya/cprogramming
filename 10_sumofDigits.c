18fe8309fe2547af62e81d9ffd8cec005c420dab
Jeevanand Maurya <jeevanandmaurya@gmail.com>
2024-10-19 20:01:10 +0530

    Update 10_sumofDigits.c
    new optimised code with help of gpt

--- 10_sumofDigits.c
+++ 10_sumofDigits.c
@@ -7,47 +7,34 @@ int main()
     int digitSum = 0;
     int digitCount = 1;
 
-    //Taking Input
+    // Taking Input
     printf("Enter a number atmost 10 digits:\n");
     scanf("%d", &number);
 
-    //Counting no of digits in given number
-    int i = 10;
-    while (number >= i)
+    // Counting no of digits in given number
+    int tempNumber = number;
+    while (tempNumber > 0)
     {
-        i = i * 10;
         digitCount += 1;
+        tempNumber /= 10;
     }
+    int digit[digitCount]; // Digits
 
-    //To get digits we divide number by 10,100,... so on and int its remainder
-    int digit[digitCount];//Digits
-    int n[digitCount];//Digits * 10 100 1000 ...
-
-    int k = 10;
-    n[0] = number % k;
-    digit[0] = n[0];
-    for (int i = 1; i < digitCount; i++)
+    tempNumber = number;
+    digitCount = 0;
+    while (tempNumber > 0)
     {
-        k = k * 10;
-        n[i] = number % k;
-        for (int j = 1; j <= i; j++)
-        {
-            n[i] = n[i] - n[i - j];
-        }
 
-        // Converting n into digits
-        digit[i] = n[i] * 10 / k;
-    }
-    //Adding Digits
-    for (int j = 0; j < digitCount; j++)
-    {
-        digitSum += digit[j];
+        digit[digitCount] = tempNumber % 10;
+        digitSum += digit[digitCount];
+        digitCount += 1;
+        tempNumber /= 10;
     }
 
-    //Printing digit count and digit sum
+    // Printing digit count and digit sum
     printf("Digit Count=%d\n", digitCount);
 
-    for (int j = digitCount - 1; j > -1; j--)
+    for (int j = digitCount - 1; j >= 0; j--)
     {
         printf("%d", digit[j]);
         if (j > 0)
