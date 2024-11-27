2e6ab0cc5f55f8cd067b5e216a1b46a257b7ac2d
Jeevanand-Github <jeevanandmaurya@gmail.com>
2024-11-27 11:12:48 +0530

    sorting by swapping

--- 15_sortingArray.c
+++ 15_sortingArray.c
@@ -4,32 +4,9 @@ int main()
 {
 
     int arr[10] = {23, -49, 553, 22, 12, 3, 0, 22, 56, -80};
-    int ascending[10];
-    int descending[10];
-
-    for (int i = 0; i < 10; i++)
-    {
-        ascending[i] = arr[0];
-    }
-
-    for (int i = 0; i < 10; i++)
-    {
-        if (arr[i] < ascending[0])
-        {
-            ascending[0] = arr[i];
-        }
-    }
-
-    for (int j = 1; j < 10; j++)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            if (arr[i] < ascending[j] && arr[i] > ascending[j - 1])
-            {
-                ascending[j] = arr[i];
-            }
-        }
-    }
+    // int ascending[10];
+    // int descending[10];
+    int swap_count=0;
 
     printf("Array:\n");
     for (int i = 0; i < 10; i++)
@@ -37,11 +14,26 @@ int main()
 
         printf(" %d ", arr[i]);
     }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            int c = arr[i + 1];
+            arr[i + 1] = arr[i];
+            arr[i] = c;
+            i = -1;
+            swap_count++;
+        }
+
+    }
+
+    printf("\nSwap Count-> %d",swap_count);
     printf("\nAscending Order Array:\n");
     for (int i = 0; i < 10; i++)
     {
 
-        printf(" %d ", ascending[i]);
+        printf(" %d ", arr[i]);
     }
 
     return 0;
